30af651c6a6140cb895aec813b4ce6a9c6d03777
Dony Cahyanto <donycahyanto99@gmail.com>
2025-02-11 09:07:33 +0700

    mengubah service menjadi certification

--- script.js
+++ script.js
@@ -58,3 +58,12 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+// Untuk serifikat
+function openModal(src) {
+  document.getElementById('modal').style.display = 'block';
+  document.getElementById('modal-pdf').src = src;
+}
+
+function closeModal() {
+  document.getElementById('modal').style.display = 'none';
+}
